cb23422b3103eb039f8bd3941f7685dc62b6263f
Eze <Mailsforeze@gmail.com>
2019-08-22 15:06:44 +0100

    adds new list ui

--- src/components/Joblist/index.vue
+++ src/components/Joblist/index.vue
@@ -1,21 +1,22 @@
 <template>
-  <div class="col-md-2">
-    <div v-for="job in jobs()" :key="job.id">     
-            <!-- <a href="#"><img alt="Image" class="img-fluid"></a> -->
+        <div class="mb-5">
+          <div v-for="job in jobs()" :key="job.id" class="row align-items-start job-item border-bottom pb-3 mb-3 pt-3">
+            <div class="col-md-2">
+              <a href="job-single.html"><img src="images/featured-listing-1.jpg" alt="Image" class="img-fluid"></a>
+            </div>
             <div class="col-md-4">
               <span class="badge badge-primary px-2 py-1 mb-3">{{job.type}}</span>
-              <h2><a :href="job.url">Apply</a> </h2>
-              <p class="meta"> : <strong>John Stewart</strong> In: <strong>{{job.title}}</strong></p>
+              <h2><a href="job-single.html">{{job.title}}</a> </h2>
+              <p class="meta"><a :href="job.url">Apply</a></p>
             </div>
             <div class="col-md-3 text-left">
-              <h4>{{job.location}}</h4>
+              <h3>Location</h3>
+              <span class="meta">{{job.location}}</span>
             </div>
             <div class="col-md-3 text-md-right">
-              <strong class="text-black">{{job.created_at}}</strong>
+              <!-- <strong class="text-black">$60k &mdash; $100k</strong> -->
             </div>
-    </div>
-
-
+          </div>
 </div>
  
 </template>
@@ -37,12 +38,10 @@ export default {
         ...mapActions(["addJobs"])
     },
     created(){
-            axios.get("https://jobs.github.com/positions.json?description=python&location=us").then((data)=>{
+       axios.get("https://jobs.github.com/positions.json?description=python&location=us").then((data)=>{
               this.addJobs(data.data)
-              
             }).catch((error)=>{console.log("unable to reach server::"+error)})
-            
-        },
+      },
         mounted(){
           if (this.jobs().length>0){console.log(this.jobs().data)}
         }
